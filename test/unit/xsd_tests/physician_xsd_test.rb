422d9c3c7b0f5454dc1f354779dce86262c9749b
kkamperschroer <kkamperschroer@gmail.com>
2012-02-22 16:55:59 -0600

    Added physician xsd and a single test.

---
+++ test/unit/xsd_tests/physician_xsd_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+require "nokogiri"
+
+class PhysicianXSDTest < ActiveSupport::TestCase
+  #paths are relative to root dir of project (agas)
+  phys1_xml = File.expand_path("test/unit/xsd_tests/test_xmls/physician_1.xml")
+  phys_xsd = File.expand_path("public/xsd/physician.xsd")
+
+  test "files_exist" do
+    assert File.exists?(phys1_xml)
+    assert File.exists?(phys_xsd)
+  end
+
+  test "valid_xml_for_schema" do
+    doc = Nokogiri::XML(File.read(phys1_xml))
+    xsd = Nokogiri::XML::Schema(File.read(phys_xsd))
+
+    xsd.validate(doc).each do |error|
+      puts error.message
+      assert false #since we have errors, we obviously didn't pass.
+    end
+
+  end
+end
