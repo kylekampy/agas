2a6db556bd22becc73d5d9f415d38d8025150ecf
kkamperschroer <kkamperschroer@gmail.com>
2012-02-22 17:23:28 -0600

    All tests passing for now. Need to figure out a better way to access "username" or figure out a reason as to why it shouldn't every be accessed from within Administrator or Physician.

--- app/models/physician.rb
+++ app/models/physician.rb
@@ -3,4 +3,8 @@ class Physician < ActiveRecord::Base
  has_many :address, :as => :owner
  has_many :phones, :as => :owner
  has_many :emails, :as => :owner
+
+  def username
+    "TODO - This logic shouldn't even be going through here, since login contains this information. Rails doesn't know how to access login from here. Only the other way around."
+  end
 end
