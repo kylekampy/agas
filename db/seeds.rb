f2c6434db1e05fd725a0c064c8f17b2ba277c0e0
Kyle Kamperschroer <kkamperschroer@gmail.com>
2012-03-29 14:14:23 -0700

    Added some stuff to db:seed file. Incomplete.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+#For creating logins
+def l(un, pass)
+  return Login.create([{ :username => un, :password => pass, :password_confirmation => pass, :owner_type => "physician" }])[0]
+end
+
+def admin(un, pass)
+  #todo
+  nil
+end
+
+#For creating physicians
+def phy(f_nm, m_nm, l_nm, specialty, office_num, login)
+  Physician.create([{ :firstname => f_nm, :middlename => m_nm, :lastname => l_nm, :specialty => specialty, :office_num => office_num, :login => login}])
+end
+
+def pat(f_nm, m_nm, l_nm, phy_id, dob, pharm_id, ins_id)
+  #todo
+  nil
+end
+
+phy("Kyle", "A", "Kamperschroer", "Nose", 62, l("kyle", "password"))
+phy("Ben", "A", "Metzger", "Eyes", 23, l("ben", "password"))
+phy("Peter", "A", "Bougie", "Ears", 24, l("peter", "password"))
+phy("Zhicheng", "A", "Fu", "Mouth", 61, l("fu", "password"))
