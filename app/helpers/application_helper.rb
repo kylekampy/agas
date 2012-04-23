b6206d19c94452862db9ffc7a4f22d0ab9f75908
Benjamin Metzger <ben@metzger.cc>
2012-04-23 16:17:27 -0500

    Made all of the names in show look better and make link to physician from medical staff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,5 @@ module ApplicationHelper
     direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
     link_to title, {:sort => column, :direction => direction}, {:class => css_class}
   end
+  
 end
